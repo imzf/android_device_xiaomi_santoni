f090d4a2bd633a069816d8cc6cffaad06985d53a
imzf <tdzfeng@gmail.com>
2019-02-11 07:08:53 +0800

    dalvik: phone-xhdpi-2048-dalvik-heap

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -74,11 +74,11 @@ persist.debug.coresight.config=stm-events
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapstartsize=8m \
 dalvik.vm.heapgrowthlimit=192m \
-dalvik.vm.heapsize=384m \
+dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=4m \
+dalvik.vm.heapsize=512m \
 dalvik.vm.heapmaxfree=8m
 
 # Display
